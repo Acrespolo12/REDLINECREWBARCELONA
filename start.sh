28b958b901bee715b1db8d135d84daae1cb7456a
Acrespolo12 <70185717+Acrespolo12@users.noreply.github.com>
2026-04-08 22:54:06 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Reemplazar puerto dinámicamente
+sed -i "s/Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
+sed -i "s/:80>/:${PORT}>/" /etc/apache2/sites-enabled/000-default.conf
+
+apache2-foreground
