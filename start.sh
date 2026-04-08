ed358cae8bc44a962d01cb0b83e644a215de08ed
Acrespolo12 <70185717+Acrespolo12@users.noreply.github.com>
2026-04-08 22:55:37 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# Reemplazar puerto dinámicamente
+# Si PORT no existe, fallback a 80
+PORT=${PORT:-80}
+
+echo "Usando puerto: $PORT"
+
+# Reemplazar configuración de Apache en runtime
 sed -i "s/Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
 sed -i "s/:80>/:${PORT}>/" /etc/apache2/sites-enabled/000-default.conf
 
